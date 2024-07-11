4623a36f932268eed8afb4567bc87aa8550e3861
arjunkr403 <arjunkr403@gmail.com>
2024-07-11 11:06:10 +0530

    projects page done

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,18 +1,36 @@
-import './projects.scss';
+import "./projects.scss";
 
 const Projects = () => {
   return (
-    <div className='project'>
+    <div className="project">
       <div className="wrapper">
-        <div className="p1" style={{background:'red'}} >yo</div>
-        <div className="p2" style={{background:'yellow'}}>let</div>
-        <div className="p3" style={{background:'green'}}>go</div>
-        <div className="p4" style={{background:'black'}}>dont</div>
-        <div className="p5" style={{background:'white'}}><span>yess</span>
+        <a href="https://arjunkr403.github.io/weather-app/" target="_blank">
+          <div className="p1">
+          <img src="src\images\weather.png" alt="" />
+          <span>Weather App</span>
         </div>
+          </a>
+        <a href="https://arjunkr403.github.io/p3-sidebar/" target="_blank">
+          <div className="p2">
+          <img src="src\images\dashboard.png" alt="" />
+          <span>Dashboard With Sidebar</span>
+        </div>
+          </a>
+        <a href="https://arjunkr403.github.io/yt-dashboard/" target="_blank">
+          <div className="p3">
+          <img src="src\images\yt.png" alt="" />
+          <span>Youtube Dashboard</span>
+        </div>
+          </a>
+        <a href="https://arjunkr403.github.io/drumset-JS/" target="_blank">
+          <div className="p4">
+          <img src="src\images\drumkit.png" alt="" />
+          <span>DrumKit</span>
+        </div>
+          </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
